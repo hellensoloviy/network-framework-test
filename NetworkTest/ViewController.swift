b9b4fee8192880703da48145720ae5b6563322ef
Hellen S <hellensoloviy@gmail.com>
2018-11-16 14:39:58 +0200

    Initial Commit

---
+++ NetworkTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NetworkTest
+//
+//  Created by Hellen Soloviy on 11/16/18.
+//  Copyright © 2018 Hellen Soloviy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
